6e34413cdeb835dd8a3ab2110ebbcd2c95843700
linlizhong <1649404329@qq.com>
2021-02-23 14:31:35 +0800

    登录页；

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -33,7 +33,7 @@
                                     v-model="password">
                             </el-input>
                         </el-form-item>
-                        <el-button type="primary" style="width: 100%">立即登录</el-button>
+                        <el-button type="primary" style="width: 100%" @click="login()">立即登录</el-button>
                     </el-form>
                 </el-card>
 
@@ -112,6 +112,34 @@
                     'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
                     'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
                 ]
+            }
+        },
+        methods: {
+            login(){
+                const _this = this;
+                let loginReq = {
+                    username: this.username,
+                    password: this.password,
+                };
+                _this.$axios.post('/post/blog/edit', loginReq, {
+                    headers: {
+                        "Authorization": localStorage.getItem("token")
+                    }
+                }).then(res => {
+                    //成功跳到博客列表页面
+                    this.$alert('操作成功', '提示', {
+                        confirmButtonText: '确定',
+                        showClose: false,
+                        callback: action => {
+                            /*this.$message({
+                                type: 'info',
+                                message: `action: ${action}`
+                            });*/
+                            this.$router.push("/blogs")
+                        }
+                    });
+                })
+                this.$router.push("/blogHome")
             }
         }
     }
